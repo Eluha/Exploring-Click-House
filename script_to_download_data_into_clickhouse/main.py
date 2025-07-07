0b9e9e94acb5a0205c87cc51722be75001851a21
Eluha <ilia.zadorniy@yandex.ru>
2025-07-07 12:33:17 +0000

    Параметризировал данные при авторизации к ClickHouse

--- script_to_download_data_into_clickhouse/main.py
+++ script_to_download_data_into_clickhouse/main.py
@@ -1,11 +1,11 @@
 from clickhouse_driver import Client
 
 client = Client(
-    host='45.8.230.101',
-    port=9000,            # порт по умолчанию для native-протокола
-    user='clickhouse',
-    password='Xv7@Dm2!pRq9#LaB',
-    database='train',  # (опционально, если не 'default')
+    host='${HOST}',
+    port='${PORT_CLICKHOUSE}',            # порт по умолчанию для native-протокола
+    user='${CLICKHOUSE_USER}',
+    password='${CLICKHOUSE_PASSWORD}',
+    database='${DATABASE_NAME}',  # (опционально, если не 'default')
 )
 
 
